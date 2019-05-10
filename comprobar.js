e4d302e54233f27fd58c8aaeea2e7fc2e29d2171
ALUMNADO\riluas <riluas@floridauniversitaria.es>
2019-05-10 11:55:04 +0200

    Añadido un poco de js

---
+++ comprobar.js
@@ -0,0 +1,16 @@
+function comp() {
+  let nombre=document.getElementById('nombre').value;
+  let apellidos=document.getElementById('apellidos').value;
+  let edad=document.getElementById('equipo').value;
+  let curso=document.getElementById('edad').value;
+  let curso=document.getElementById('NCanastas').value;
+  var error=true;
+  document.getElementById('nombreOculto').style.display = 'none';
+  document.getElementById('apellidoOculto').style.display = 'none';
+  document.getElementById('Equipo0culto').style.display = 'none';
+  document.getElementById('edadOculto').style.display = 'none';
+  document.getElementById('numOculto1').style.display = 'none';
+  document.getElementById('NCanastasOculto').style.display = 'none';
+  document.getElementById('numOculto2').style.display = 'none';
+
+}
