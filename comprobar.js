12fef2a4310d8fbdb3f47d7c4d6248912321487a
ALUMNADO\riluas <riluas@floridauniversitaria.es>
2019-05-10 12:00:17 +0200

    completado js

--- comprobar.js
+++ comprobar.js
@@ -1,4 +1,4 @@
-function comp() {
+function comprobar() {
   let nombre=document.getElementById('nombre').value;
   let apellidos=document.getElementById('apellidos').value;
   let equipo=document.getElementById('equipo').value;
@@ -13,4 +13,35 @@ function comp() {
   document.getElementById('NCanastasOculto').style.display = 'none';
   document.getElementById('numOculto2').style.display = 'none';
 
+  if(nombre==""){
+      document.getElementById('nombreOculto').style.display = 'block';
+      error=false;
+    }
+    if (apellidos=="") {
+      document.getElementById('apellidoOculto').style.display = 'block';
+      error=false;
+    }
+    if (equipo=="") {
+      document.getElementById('Equipo0culto').style.display = 'block';
+      error=false;
+    }
+    if (edad=="") {
+        document.getElementById('edadOculto').style.display = 'block';
+        error=false;
+    }
+    if (isNaN(edad)) {
+      document.getElementById('numOculto1').style.display = 'block';
+      error=false;
+    }
+  if (canastas=="") {
+      document.getElementById('NCanastasOculto').style.display = 'block';
+      error=false;
+  }
+    if (isNaN(canastas)) {
+      document.getElementById('numOculto2').style.display = 'block';
+      error=false;
+    }
+    if (error==false) {
+      return false;
+    }
 }
